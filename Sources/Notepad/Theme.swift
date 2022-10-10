97b859fbd075d9d3bfc51c294e4a0221e38aea14
Justin LeBlanc <leblanc@wustl.edu>
2022-10-10 15:46:08 -0500

    We're only supporting two themes, so remove the other ones from the enum

--- Sources/Notepad/Theme.swift
+++ Sources/Notepad/Theme.swift
@@ -15,16 +15,8 @@
 public struct Theme {
 
     public enum BuiltIn: String {
-        case Base16TomorrowDark = "base16-tomorrow-dark"
-        case Base16TomorrowLight = "base16-tomorrow-light"
-        case BluesClues = "blues-clues"
-        case OneDarkCustom = "one-dark-custom"
-        case OneLightCustom = "one-light-custom"
         case OneDark = "one-dark"
         case OneLight = "one-light"
-        case SolarizedDark = "solarized-dark"
-        case SolarizedLight = "solarized-light"
-        case SystemMinimal = "system-minimal"
 
         public func theme() -> Theme {
             return Theme(self.rawValue)
